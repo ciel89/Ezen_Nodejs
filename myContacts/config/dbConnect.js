ed85a3a88f7a16de7284ce435d5183b7a10d6285
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-08-04 20:28:15 +0900

    Update dbConnect.js

--- myContacts/config/dbConnect.js
+++ myContacts/config/dbConnect.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-    const connect = await mongoose.connect(process.env.DB_CONNECT);
-    console.log("DB connected");
+
   } catch (err) {
-    console.log(err);
+
   }
 };
 
