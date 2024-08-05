8fd6acc875b301168cc15e7e50a52c9563570fe3
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-08-05 17:10:57 +0900

    Add files via upload

--- myContacts/models/contactModel.js
+++ myContacts/models/contactModel.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 //스키마 정의
-
-
-
+const contactSchema = new mongoose.Schema(
+    {
+        name : {type:String,
+                required : true
+        },
+        email : {type:String
+        },
+        phone : {type:String,
+                required :[true, '전화번호는 꼭 쓰세요']
+        }
+    },
+    {
+        timestamps : true
+    }
+);
 
 //스카마 => 모델
-//mongoose.model(모델명, 스키마명)
+//mongoose.model(모델명-대문자로...., 스키마명)
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
