a6c3068842156810e865fc2600d247be5c780ee6
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-08-05 17:10:19 +0900

    Update routes.js

--- myContacts/routes.js
+++ myContacts/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {getAllContacts, createContact,  getContact,  updateContact,  deleteContact } = require('./controllers/contactController');
 
-router.route("/").get(getAllContacts);
+router.route("/").get(getAllContacts).post(createContact);;
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
