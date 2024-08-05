4c6d67926c501d330d7c8ae875ec5bc728c2f61a
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-08-05 17:07:45 +0900

    Add files via upload

--- myContacts/config/dbConnect.js
+++ myContacts/config/dbConnect.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-
+    const connect = await mongoose.connect(process.env.DB_CONNECT);
+    console.log('DB connected')
   } catch (err) {
-
+    console.log(err);
   }
 };
 
